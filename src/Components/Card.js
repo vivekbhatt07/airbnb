8a7828a2c4dd84a67f2be57b984b756686ae876c
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-27 23:06:10 +0530

    Fixed: Images

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,26 +3,22 @@ import Star from "../Assets/Star.svg";
 export default function Card(props) {
   console.log(props);
   return (
-      <div className="card">
-        <img
-          className="card__img"
-          src={require(`../Assets/Athlete.svg`).default}
-          alt="dynamic"
-        />
-        <div className="card__content">
-          <div className="card__stats">
-            <img className="card__star" src={Star} alt="star" />
-            <span className="card__rating">{props.item.stats.rating}</span>
-            <span className="card__reviewCount">
-              ({props.item.stats.reviewCount}).
-            </span>
-            <span className="card__location">{props.item.location}</span>
-          </div>
-          <div className="card__text">{props.item.title}</div>
-          <div className="card__price">
-            <strong>From ${props.item.price}</strong> / person
-          </div>
+    <div className="card">
+      <img className="card__img" src={props.item.coverImg} alt="dynamic" />
+      <div className="card__content">
+        <div className="card__stats">
+          <img className="card__star" src={Star} alt="star" />
+          <span className="card__rating">{props.item.stats.rating}</span>
+          <span className="card__reviewCount">
+            ({props.item.stats.reviewCount}).
+          </span>
+          <span className="card__location">{props.item.location}</span>
+        </div>
+        <div className="card__text">{props.item.title}</div>
+        <div className="card__price">
+          <strong>From ${props.item.price}</strong> / person
         </div>
       </div>
+    </div>
   );
 }
