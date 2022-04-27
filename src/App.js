466b1f92c522a825f836a55639c4a443adc7427b
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-27 12:47:50 +0530

    Added: Dynamic Data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
-
+import Data from "./Data";
 export default function App() {
+  const displayCards = Data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <div className="card_container">{displayCards}</div>
     </>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,24 +1,28 @@
 import React from "react";
-import Athlete from "../Assets/Athlete.svg";
-// import Bike from "../Assets/Bike.svg";
 import Star from "../Assets/Star.svg";
-// import Wedding from "../Assets/Wedding.svg";
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
   return (
-    <div className="card">
-      <img className="card__img" src={Athlete} alt="dynamic" />
-      <div className="card__content">
-        <div className="card__stats">
-          <img className="card__star" src={Star} alt="star" />
-          <span className="card__rating">5.0</span>
-          <span className="card__reviewCount">(6).</span>
-          <span className="card__location">USA</span>
-        </div>
-        <div className="card__text">Life lessons with Katie Xaleres</div>
-        <div className="card__price">
-          <strong>From $136</strong> / person
+      <div className="card">
+        <img
+          className="card__img"
+          src={require(`../Assets/Athlete.svg`).default}
+          alt="dynamic"
+        />
+        <div className="card__content">
+          <div className="card__stats">
+            <img className="card__star" src={Star} alt="star" />
+            <span className="card__rating">{props.item.stats.rating}</span>
+            <span className="card__reviewCount">
+              ({props.item.stats.reviewCount}).
+            </span>
+            <span className="card__location">{props.item.location}</span>
+          </div>
+          <div className="card__text">{props.item.title}</div>
+          <div className="card__price">
+            <strong>From ${props.item.price}</strong> / person
+          </div>
         </div>
       </div>
-    </div>
   );
 }
